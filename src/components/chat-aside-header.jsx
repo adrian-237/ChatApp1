5f0df6cdbb65b5822a36f4ed85e6e37456c2e945
Adrian <boronciuc.adrian2@gmail.com>
2024-08-09 14:07:43 +0300

    vite config base changed

---
+++ src/components/chat-aside-header.jsx
@@ -0,0 +1,7 @@
+export default function ChatAsideHeader() {
+  return (
+    <div className="chat-aside-header">
+      <img src="./user.jpg" alt="" />
+    </div>
+  );
+}
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://adrian-237.github.io/ChatApp1/",
+  base: "/ChatApp1/",
 });
